f9b3d5aecb9a1b9e00450045049bf77e29bbe50a
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2023-12-29 14:51:19 +0700

    first commit

---
+++ src/components/Home/ProductsStatistics.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProductsStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Products statistics</h5>
+          <iframe
+            title="Products statistics"
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-shoeshop-rjhhu/embed/charts?id=64f98f43-5572-4ce5-8701-59fcef09c601&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsStatistics;
---
+++ src/components/Home/SalesStatistics.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SaleStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Sale statistics</h5>
+          <iframe
+            title="sale statistics"
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-shoeshop-rjhhu/embed/charts?id=64f98911-a893-4aa5-8302-4deed3ffb1a4&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default SaleStatistics;
